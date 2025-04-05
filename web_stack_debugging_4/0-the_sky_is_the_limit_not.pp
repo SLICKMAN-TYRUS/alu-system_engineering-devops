4b57ef8027567dce029ebbe01a07e34b5d1c23f4
SLICKMAN-TYRUS <a.chol@alustudent.com>
2025-04-05 11:22:14 +0300

    0-the_sky_is_the_limit_not.pp

--- web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
+++ web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
@@ -4,4 +4,4 @@ exec { 'ULIMIT':
   onlyif  => 'test -e /etc/default/nginx',
   command => 'sed -i "5s/[0-9]\+/$( ulimit -n )/" /etc/default/nginx; service nginx restart',
   path    => '/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin',
-}web_stack_debugging_4
+}
